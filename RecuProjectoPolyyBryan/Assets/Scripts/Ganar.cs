d08925f6cde55d1afd660b800706a66ee711ff98
chaosboy99 <brayanguanangamorales@gmail.com>
2024-07-13 00:17:26 +0200

    EscenaWin

---
+++ RecuProjectoPolyyBryan/Assets/Scripts/Ganar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ganar : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pj"))
+        {
+            SceneManager.LoadScene(4);
+        }
+
+    }
+}
